9259381404ce4e539a1bfc2ca9580278e8d24b8e
Scumbrum <73284043+Scumbrum@users.noreply.github.com>
2021-02-11 21:57:16 +0200

    lowercase2

---
+++ src/arrayList/MyArray.java
@@ -0,0 +1,199 @@
+package arrayList;
+
+
+import java.util.Iterator;
+
+public class MyArray<E> implements Iterable<E>, List {
+	private int size = 10;
+	private Object[] array = new Object[size];
+	private int loadfactor = 1/3;
+	private int current = 0;
+	
+	private class ArrIterator<E> implements Iterator<E>{
+		int index=0;
+		@Override
+		public boolean hasNext() {
+			if(index<current) {
+				return true;
+			}
+			return false;
+		}
+
+		@Override
+		public E next() {
+			return (E) array[index++];
+		}
+	}
+	
+	
+	
+	private void changeSize(int size) {
+		this.size=size;
+		Object[] temparr = new Object[size];
+		System.arraycopy(this.array, 0, temparr, 0, this.current);
+		this.array=temparr;
+	}
+	
+	@Override
+	public int sumInt(){
+        if (this.current == 0){
+            return 0;
+        }
+        try {
+        	int element = (Integer) this.array[0];
+        }
+        catch(ClassCastException e) {
+        	throw new IllegalArgumentException("Масив не містить чисел");
+        }
+        int result = 0;
+        for(int i=0;i<current;i++) {
+        	int number = (Integer) array[i];
+        	result+=number;
+        }
+        return result;
+    }
+	
+	@Override
+	public void setEnd (Object setObject) {
+		try {
+			E element = (E) setObject;
+		if(this.current>=this.size-1) {
+			changeSize(this.size*2);
+		}
+		array[this.current++]=element;
+		}
+		catch(ClassCastException e) {
+			throw new IllegalArgumentException("Не вірне значення");
+		}
+	}
+	
+	@Override
+	public void insert(Object obj, Integer index) {
+		if(index>this.current) {
+			throw new IndexOutOfBoundsException("Ви вийшли за границі масиву");
+		}
+		try {
+			E element = (E) obj;
+		if(this.current>=this.size-1) {
+			this.size=this.size*2;
+			Object[] temparr = new Object[size];
+			for(int i=0;i<index;i++) {
+				temparr[i]=this.array[i];
+			}
+			temparr[index]=element;
+			for(int i =index+1;i<=this.current;i++) {
+				temparr[i]=this.array[i-1];
+			}
+			this.array=temparr;
+		}
+		else {
+			for(int i=current;i>index;i--) {
+				this.array[i]=this.array[i-1];
+			}
+			this.array[index]=element;
+		}
+		this.current++;
+		}
+		catch (ClassCastException e) {
+			throw new IllegalArgumentException("Не вірне значення");
+		}
+	}
+	
+	@Override
+	public void delete(Integer index){
+		if(index>=this.current) {
+			throw new IndexOutOfBoundsException("Ви вийшли за границі масиву");
+		}
+		for(int i=index;i<this.current;i++) {
+			this.array[i]=this.array[i+1];
+		}
+		this.current--;
+		if (this.current>10 && this.current<=this.loadfactor*this.size) {
+			changeSize(this.size/2);
+		}
+	}
+	
+	@Override
+	public void set(Object elem, Integer index) {
+		try {
+			E element= (E) elem;
+		if(index>=this.current) {
+			throw new IndexOutOfBoundsException("Ви вийшли за границі масиву");
+		}
+		if(index<this.current) {
+			this.array[index]=element;
+		}
+		} catch(ClassCastException e) {
+    	throw new IllegalArgumentException("Не вірне значення");
+		}
+	}
+	
+	public int getSize() {
+		return current;
+	}
+	
+	@Override
+	public Integer getIndex(Object object) {
+		try {
+			E element= (E) object;
+		for(int i =0;i<current;i++) {
+			if(array[i]==object) {
+				return i;
+			}
+		}
+		throw new IllegalArgumentException("Заданий недійсний елемент");
+		} catch(ClassCastException e) {
+    	throw new IllegalArgumentException("Не вірне значення");
+		}
+	}
+	
+	@Override
+	public Iterator<E> iterator() {
+		return new ArrIterator<E>();
+	}
+	
+	@Override
+	public String toString() {
+		StringBuffer buffer = new StringBuffer();
+		buffer.append("[");
+		for(int i=0;i<this.current;i++) {
+			E element =(E) this.array[i];
+			buffer.append(element.toString());
+			buffer.append(", ");
+		}
+		buffer.delete(buffer.length()-2, buffer.length());
+		buffer.append("]");
+		return buffer.toString();
+	}
+
+	@Override
+	public void insertToStart(Object insertObject) {
+		this.insert(insertObject, 0);
+	}
+
+	@Override
+	public void insertToEnd(Object insertObject) {
+		this.insert(insertObject, this.current);	
+	}
+
+	@Override
+	public void deleteFromStart() {
+		this.delete(0);
+		
+	}
+
+	@Override
+	public void deleteFromEnd() {
+		this.delete(current);
+		
+	}
+
+	@Override
+	public void setStart(Object setObject) {
+		this.set(setObject, 0);
+		
+	}
+
+
+	
+}
